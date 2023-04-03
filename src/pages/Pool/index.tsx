8f5fe96b488b3f4255586cdcfa1d60670a41a0c5
cherry_mini <184675509@qq.com>
2023-04-03 18:19:03 +0800

    feat: add positions

--- src/pages/Pool/index.tsx
+++ src/pages/Pool/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import PageWrapper from '@components/Layout/PageWrapper';
 import BorderBox from '@components/Box/BorderBox';
+import Button from '@components/Button';
 import useI18n from '@hooks/useI18n';
 
 const transitions = {
   en: {
-    pool: 'Pool',
+    pools: 'Pools',
+    new_position: 'New Position',
   },
   zh: {
-    pool: '池子',
+    pools: '池子',
+    new_position: '新仓位',
   },
 } as const;
 
@@ -16,9 +19,13 @@ const PoolPage: React.FC = () => {
   const i18n = useI18n(transitions);
   return (
     <PageWrapper className="pt-56px">
-      <BorderBox className="relative mx-auto max-w-572px p-16px rounded-28px" variant="gradient-white">
+      <div className="flex justify-between mb-16px px-88px h-40px">
+        <span className="mt-10px h-30px leading-30px text-24px text-orange-normal">{i18n.pools}</span>
+        <Button className="h-40px text-18px px-24px rounded-100px" color="gradient">+ {i18n.new_position}</Button>
+      </div>
+      <BorderBox className="relative mx-auto max-w-800px p-16px rounded-28px" variant="gradient-white">
         <div className="mb-16px flex justify-between items-center pr-8px">
-          <span className="w-84px h-40px leading-40px rounded-100px text-center text-14px text-black-normal font-medium bg-orange-light-hover">{i18n.pool}</span>
+          <span className="w-84px h-40px leading-40px rounded-100px text-center text-14px text-black-normal font-medium bg-orange-light-hover">{i18n.pools}</span>
         </div>
       </BorderBox>
     </PageWrapper>
