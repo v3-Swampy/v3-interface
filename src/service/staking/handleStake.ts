8f63b6692eaf06ca2d56a712a7c699cef3876e3d
mleskesel <mleskesel@gmail.com>
2023-04-05 14:47:27 +0800

    feat(stake):WIP-unstake

--- src/service/staking/handleStake.ts
+++ src/service/staking/handleStake.ts
@@ -1,11 +1,38 @@
 import { sendTransaction } from '@service/account';
 import { VotingEscrowContract } from '@contracts/index';
+import waitAsyncResult, { isTransactionReceipt } from '@utils/waitAsyncResult';
+import { showToast } from '@components/showPopup';
+import { toI18n, compiled } from '@hooks/useI18n';
 
-export const handleStakingVST = async ({ methodName, methodParams }: { methodName: string; methodParams: any[] }) => {
+const transitions = {
+  en: {
+    success_tip: 'UnStake Successfully!',
+  },
+  zh: {
+    success_tip: '提取成功!',
+  },
+} as const;
+
+export const handleStakingVST = async ({ methodName, methodParams }: { methodName: string; methodParams?: any[] }) => {
   const txHash = await sendTransaction({
-    data: VotingEscrowContract.func.encodeFunctionData(methodName, methodParams),
+    data: VotingEscrowContract.func.encodeFunctionData(methodName, methodParams ?? []),
     to: VotingEscrowContract.address,
   });
 
   return txHash;
+};
+
+export const handleUnStake = async () => {
+  const i18n = toI18n(transitions);
+  const txHash = await sendTransaction({
+    data: VotingEscrowContract.func.encodeFunctionData('withdraw'),
+    to: VotingEscrowContract.address,
+  });
+
+  const [receiptPromise] = waitAsyncResult({ fetcher: () => isTransactionReceipt(txHash) });
+  await receiptPromise;
+
+  showToast(i18n.success_tip, {
+    type: 'success',
+  });
 };
