019056e8bc58b9027a040a694bcab1990fe6bf12
cherry-mini <184675509@qq.com>
2023-04-08 18:57:33 +0800

    feat: update detail

--- src/modules/TokenPair/index.tsx
+++ src/modules/TokenPair/index.tsx
@@ -1,19 +1,15 @@
-import React, { Suspense, useMemo } from 'react';
+import React from 'react';
+import { type PositionForUI } from '@service/pool-manage';
 import { FeeAmount } from '@service/pairs&pool';
 
-const TokenPair: React.FC<{ quoteTokenSymbol?: string; quoteTokenLogo?: string; baseTokenSymbol?: string; baseTokenLogo?: string; fee: FeeAmount }> = ({
-  quoteTokenSymbol,
-  quoteTokenLogo,
-  baseTokenSymbol,
-  baseTokenLogo,
-  fee,
-}) => {
+const TokenPair: React.FC<{ position: PositionForUI }> = ({position}) => {
+  const {leftToken, rightToken, fee} = position
   return (
     <div className="flex items-center">
-      <img className="w-24px h-24px" src={quoteTokenLogo} alt={`${quoteTokenSymbol} icon`} />
-      <img className="w-24px h-24px -ml-8px" src={baseTokenLogo} alt={`${baseTokenSymbol} icon`} />
+      <img className="w-24px h-24px" src={leftToken?.logoURI} alt={`${leftToken?.logoURI} icon`} />
+      <img className="w-24px h-24px -ml-8px" src={rightToken?.logoURI} alt={`${rightToken?.logoURI} icon`} />
       <span className="mx-4px text-14px text-black-normal font-medium">
-        {quoteTokenSymbol} / {baseTokenSymbol}
+        {leftToken?.symbol} / {rightToken?.symbol}
       </span>
       <span className="inline-block px-8px h-20px leading-20px rounded-100px bg-orange-light text-center text-14px text-orange-normal font-medium">{fee / 10000}%</span>
     </div>
