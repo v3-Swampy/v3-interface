b12bcb9647033cda3e52b6cee841f0d82df04b63
mleskesel <mleskesel@gmail.com>
2023-04-15 10:23:58 +0800

    feat(farming): boosting factor

--- src/service/staking/stakingData.ts
+++ src/service/staking/stakingData.ts
@@ -96,3 +96,12 @@ export const useUserInfo = () => {
   const lockedAmount = account ? new Unit(userInfo?.[0].toString()).toDecimalStandardUnit(0, Number(vstDecimals)) : 0;
   return [lockedAmount, userInfo?.[1]?.toString()];
 };
+/**
+ * calculate the boosting factor
+ */
+export const useBoostFactor=()=>{
+  const [lockedVstAmount]=useUserInfo()
+  let veVSTTotalSupply = useRecoilValue(escrowTotalSupplyQuery);
+  //boosting factor = (67% * <amout of veVST> /<total supply of veVST> + 33%) / 33% 
+  return Number(veVSTTotalSupply)!=0?(new Unit(0.67).mul(lockedVstAmount).div(veVSTTotalSupply).add(0.33).div(0.33).toDecimalMinUnit(1)):1
+}
