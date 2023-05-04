67b3be213b8466ff5935aeb761a6e3202b9f8fda
Anadi <sssensational.3@gmail.com>
2023-05-04 10:57:20 +0800

    Change Confirm Modal reject tip

--- src/modules/ConfirmTransactionModal/index.tsx
+++ src/modules/ConfirmTransactionModal/index.tsx
@@ -140,7 +140,7 @@ const ConfirmTransactionModal: React.FC<CommonProps & { children?: ReactNode | (
         <div className="absolute left-0 w-full top-1/2 -translate-y-1/2 text-center whitespace-nowrap">
           <FailedIcon className="mx-auto block w-70px h-70px mb-80px" />
           <p className="leading-28px text-22px text-black-normal">Transaction Rejected</p>
-          <p className="mt-16px text-center leading-18px text-14px text-gray-normal font-medium">You cancel the transcation in wallet</p>
+          <p className="mt-16px text-center leading-18px text-14px text-gray-normal font-medium opacity-0 pointer-events-none">placeHolder</p>
         </div>
         <Button color="orange" className="absolute left-16px bottom-16px right-16px h-48px rounded-100px text-16px !font-bold" onClick={hidePopup}>
           Close
