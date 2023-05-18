b3accd9dfc46e04267676ee20775ddc627714a6c
gnik-youngman <mag1c_shen_09@hotmail.com>
2023-05-18 11:40:10 +0800

    feat: delete avatarContainerRef

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,12 +1,10 @@
-import { memo, useRef, forwardRef, type ComponentProps } from 'react';
+import { memo, forwardRef, type ComponentProps } from 'react';
 import { generateAvatarURL } from '@cfx-kit/wallet-avatar';
 import composeRef from '@utils/composeRef';
 
 const Avatar = forwardRef<HTMLDivElement, ComponentProps<'div'> & { account: string; size: number }>(({ account, size, ...props }, ref) => {
-  const avatarContainerRef = useRef<HTMLDivElement>(null!);
-
   return (
-    <div className="avatar" {...props} style={{ width: size, height: size, ...props.style }} ref={composeRef(avatarContainerRef, ref)}>
+    <div className="avatar" {...props} style={{ width: size, height: size, ...props.style }} ref={composeRef(ref)}>
       {account && (
         <div className="w-full h-full rounded-[50px] overflow-hidden">
           <img className="pointer-events-none select-none" src={generateAvatarURL(account)} />
