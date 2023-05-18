8b566789404163d0432a6b267aa6ecacd085d803
gnik-youngman <mag1c_shen_09@hotmail.com>
2023-05-18 11:41:40 +0800

    feat: simplify ref

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,10 +1,9 @@
 import { memo, forwardRef, type ComponentProps } from 'react';
 import { generateAvatarURL } from '@cfx-kit/wallet-avatar';
-import composeRef from '@utils/composeRef';
 
 const Avatar = forwardRef<HTMLDivElement, ComponentProps<'div'> & { account: string; size: number }>(({ account, size, ...props }, ref) => {
   return (
-    <div className="avatar" {...props} style={{ width: size, height: size, ...props.style }} ref={composeRef(ref)}>
+    <div className="avatar" {...props} style={{ width: size, height: size, ...props.style }} ref={ref}>
       {account && (
         <div className="w-full h-full rounded-[50px] overflow-hidden">
           <img className="pointer-events-none select-none" src={generateAvatarURL(account)} />
