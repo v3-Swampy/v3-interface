2bc6a97559c13dc0687b6fa16aaabf97765cfae8
gnik-youngman <mag1c_shen_09@hotmail.com>
2023-05-18 11:35:09 +0800

    feat: use @cfx-kit/wallet-avatar

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,36 +1,19 @@
-import { memo, useRef, useLayoutEffect, forwardRef, type ComponentProps } from 'react';
-import jazzIcon from './jazzIcon';
+import { memo, useRef, forwardRef, type ComponentProps } from 'react';
+import { generateAvatarURL } from '@cfx-kit/wallet-avatar';
 import composeRef from '@utils/composeRef';
-import './index.css';
-
-function removeAllChild(dom: Element | null): void {
-  let child = dom?.lastElementChild;
-  while (child) {
-    dom?.removeChild(child);
-    child = dom?.lastElementChild;
-  }
-}
-
-function addressToNumber(address: string | null | undefined): number | undefined {
-  if (!address) {
-    return undefined;
-  }
-  const addr = address.slice(2, 10);
-  const seed = parseInt(addr, 16);
-  return seed;
-}
 
 const Avatar = forwardRef<HTMLDivElement, ComponentProps<'div'> & { account: string; size: number }>(({ account, size, ...props }, ref) => {
   const avatarContainerRef = useRef<HTMLDivElement>(null!);
 
-  useLayoutEffect(() => {
-    removeAllChild(avatarContainerRef.current);
-    if (!account) return;
-    const avatarDom = jazzIcon(size, addressToNumber(account));
-    avatarContainerRef.current.appendChild(avatarDom);
-  }, [size, account]);
-
-  return <div className="avatar" {...props} style={{ width: size, height: size, ...props.style }} ref={composeRef(avatarContainerRef, ref)} />;
+  return (
+    <div className="avatar" {...props} style={{ width: size, height: size, ...props.style }} ref={composeRef(avatarContainerRef, ref)}>
+      {account && (
+        <div className="w-full h-full rounded-[50px] overflow-hidden">
+          <img className="pointer-events-none select-none" src={generateAvatarURL(account)} />
+        </div>
+      )}
+    </div>
+  );
 });
 
 export default memo(Avatar);
