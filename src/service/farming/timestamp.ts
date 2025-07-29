9df4d04137af99bfa103cec7cc3543a92e92cff6
cherry-mini <184675509@qq.com>
2025-07-29 12:14:02 +0800

    fix: timestamp get per 10s

--- src/service/farming/timestamp.ts
+++ src/service/farming/timestamp.ts
@@ -25,6 +25,6 @@ export const useAutoRefreshTimestamp = () =>
   useAutoRefreshData({
     recoilValue: timestampSelector,
     fetcher: fetchTimestamp,
-    interval: 1000,
+    interval: 10000,
     refreshImmediately: true,
   });
