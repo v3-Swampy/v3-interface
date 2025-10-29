fb5287c6b7e672126c888378fe4c3281b6879e3a
cherry-mini <184675509@qq.com>
2025-10-29 11:35:34 +0800

    feat: increase liquidity

--- src/service/earn/increasePosition.ts
+++ src/service/earn/increasePosition.ts
@@ -1,5 +1,5 @@
 import { Unit } from '@cfxjs/use-wallet-react/ethereum';
-import { NonfungiblePositionManager } from '@contracts/index';
+import { AutoPositionManager } from '@contracts/index';
 import { getWrapperTokenByAddress } from '@service/tokens';
 import { getAccount, sendTransaction } from '@service/account';
 import { getPool } from '@service/pairs&pool';
@@ -63,7 +63,7 @@ export const handleClickSubmitIncreasePositionLiquidity = async ({
       token1AmountUnit.toDecimalMinUnit()
     );
 
-    const dataWithoutCFX = NonfungiblePositionManager.func.interface.encodeFunctionData('increaseLiquidity', [
+    const data = AutoPositionManager.func.interface.encodeFunctionData('increaseLiquidity', [
       {
         tokenId,
         amount0Desired: Unit.fromStandardUnit(token0Amount, token0.decimals).toHexMinUnit(),
@@ -73,16 +73,13 @@ export const handleClickSubmitIncreasePositionLiquidity = async ({
         deadline: getDeadline(),
       },
     ]);
-    const dataWithCFX = NonfungiblePositionManager.func.interface.encodeFunctionData('multicall', [
-      [dataWithoutCFX, NonfungiblePositionManager.func.interface.encodeFunctionData('refundETH')],
-    ]);
 
     const hasWCFX = token0.symbol === 'WCFX' || token1.symbol === 'WCFX';
 
     const transactionParams = {
       value: hasWCFX ? Unit.fromStandardUnit(token0.symbol === 'WCFX' ? token0Amount : token1Amount, 18).toHexMinUnit() : '0x0',
-      data: !hasWCFX ? dataWithoutCFX : dataWithCFX,
-      to: NonfungiblePositionManager.address,
+      data: data,
+      to: AutoPositionManager.address,
     };
 
     const recordParams = {
