d8682b431cf5059b3c7339ffa8879384bfa97ef3
ddddefiiii <mmibuy@outlook.com>
2025-11-06 14:40:57 +0800

    fix: not reset config in new position

--- src/pages/Earn/index.tsx
+++ src/pages/Earn/index.tsx
@@ -3,15 +3,13 @@ import PageWrapper from '@components/Layout/PageWrapper';
 import useI18n from '@hooks/useI18n';
 import { Suspense } from 'react';
 import Spin from '@components/Spin';
-import { Link, NavLink, Outlet, useNavigate } from 'react-router-dom';
+import { Link, NavLink, Outlet } from 'react-router-dom';
 import Button from '@components/Button';
 import { ReactComponent as AddIcon } from '@assets/icons/add.svg';
 import BorderBox from '@components/Box/BorderBox';
 import Switch from '@components/Switch';
 import FarmIcon from '@assets/imgs/farm.png';
 import { useFarmsOnly } from '@service/earn';
-import { resetToken } from './AddLiquidity/SelectPair';
-import { setCurrentFee } from './AddLiquidity/SelectFeeTier';
 
 const transitions = {
   en: {
@@ -19,7 +17,7 @@ const transitions = {
     desc: 'Concentrated and effective liquidity providers will get more rewards.',
     myPositions: 'My Positions',
     allPools: 'All Pools',
-    new_positions: 'New Positions',
+    new_position: 'New Position',
     farms_only: 'Farms only',
   },
   zh: {
@@ -27,14 +25,13 @@ const transitions = {
     desc: 'Concentrated and effective liquidity providers will get more rewards.',
     myPositions: 'My Positions',
     allPools: 'All Pools',
-    new_positions: '新仓位',
+    new_position: '新仓位',
     farms_only: 'Farms only',
   },
 } as const;
 
 const EarnPage: React.FC = () => {
   const [onlyFarms, setOnlyFarms] = useFarmsOnly();
-  const navigate = useNavigate();
   const i18n = useI18n(transitions);
   const buttonClass =
     'inline-block no-underline py-10px leading-18px px-6 rounded-full text-center text-sm font-normal border border-solid text-gray-normal box-border cursor-pointer';
@@ -47,41 +44,23 @@ const EarnPage: React.FC = () => {
           <div className="flex-1 flex gap-20px items-end lt-mobile:flex-col lt-mobile:items-start">
             <div className="flex items-center lt-mobile:w-full">
               <div className="leading-30px text-24px text-orange-normal font-normal lt-mobile:text-18px lt-mobile:leading-23px flex-1">{i18n.earn}</div>
-              <div className="no-underline shrink-0 hidden lt-mobile:block">
-                <Button
-                  color="gradient"
-                  className="px-24px h-40px text-14px rounded-100px"
-                  id="earn-add-new-position"
-                  onClick={() => {
-                    resetToken();
-                    setCurrentFee(3000);
-                    navigate('/earn/add_liquidity');
-                  }}
-                >
+              <Link to="/earn/add_liquidity" className="no-underline shrink-0 hidden lt-mobile:block">
+                <Button color="gradient" className="px-24px h-40px text-14px rounded-100px" id="earn-add-new-position">
                   <AddIcon className="mr-8px w-12px h-12px" />
-                  {i18n.new_positions}
+                  {i18n.new_position}
                 </Button>
-              </div>
+              </Link>
             </div>
             <div className="font-normal text-14px leading-18px mb-2px color-gray-normal lt-mobile:text-12px lt-mobile:leading-15px lt-mobile:ml-0 lt-mobile:mt-6.5px">
               {i18n.desc}
             </div>
           </div>
-          <div className="no-underline shrink-0 lt-mobile:hidden">
-            <Button
-              color="gradient"
-              className="px-24px h-40px text-14px rounded-100px"
-              id="earn-add-new-position"
-              onClick={() => {
-                resetToken();
-                setCurrentFee(3000);
-                navigate('/earn/add_liquidity');
-              }}
-            >
+          <Link to="/earn/add_liquidity" className="no-underline shrink-0 lt-mobile:hidden">
+            <Button color="gradient" className="px-24px h-40px text-14px rounded-100px" id="earn-add-new-position">
               <AddIcon className="mr-8px w-12px h-12px" />
-              {i18n.new_positions}
+              {i18n.new_position}
             </Button>
-          </div>
+          </Link>
         </div>
 
         <BorderBox className="relative w-full p-16px pt-24px rounded-28px group lt-mobile:rounded-4" variant="gradient-white">
