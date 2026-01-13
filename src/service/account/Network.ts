40f9a8079689d5aa75239455c396eef8256305a8
ddddefiiii <mmibuy@outlook.com>
2026-01-13 14:11:01 +0800

    feat: .net

--- src/service/account/Network.ts
+++ src/service/account/Network.ts
@@ -4,7 +4,7 @@ export const Network = {
   chainId: isProduction ? '1030' : '71',
   chainName: isProduction ? 'Conflux eSpace' : 'Conflux eSpace (Testnet)',
   rpcUrls: [isProduction ? 'https://evm.confluxrpc.com' : 'https://evmtestnet.confluxrpc.com'],
-  blockExplorerUrls: [isProduction ? 'https://evm.confluxscan.net' : 'https://evmtestnet.confluxscan.net'],
+  blockExplorerUrls: [isProduction ? 'https://evm.confluxscan.org' : 'https://evmtestnet.confluxscan.org'],
   nativeCurrency: {
     name: 'Conflux',
     symbol: 'CFX',
